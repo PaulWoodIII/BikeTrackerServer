de9aec42b2bacfa399f2d4831576314d29cfeaf8
Paul Wood III <paul.wood.iii@gmail.com>
2013-03-13 20:02:13 +0800

    mongo logic changed

--- routes/index.js
+++ routes/index.js
@@ -5,28 +5,25 @@
 
 var mongo = require('mongodb');
 
-var Server = mongo.Server,
-   Db = mongo.Db,
-   BSON = mongo.BSONPure;
+var uri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost:27017/biketrackerdb'; 
 
-var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'localhost'; 
-	
-var server = new Server(mongoUri,27017,{auto_reconnect: true});
-db = new Db('biketrackerdb', server, {safe: true});
-
-db.open(function(err, db) {
-   if(!err) {
-       console.log("Connected to 'biketrackerdb' database");
-       db.collection('locations', {safe:true}, function(err, collection) {
-           if (err) {
-               console.log("The 'Locations' collection doesn't exist. Creating it with sample data...");
-               populateDB();
-           }
-       });
-   }
-	 else{
-		 console.log(err);
-	 }
+mongo.connect(uri,function(err,db){
+	if(err) {
+		console.log(err);
+		return;
+	}
+	else{
+		console.log('no error');
+	}
+  db.collection('locations', {safe:true}, function(err, collection) {
+      if (err) {
+          console.log("The 'Locations' collection doesn't exist. Creating it with sample data...");
+          populateDB();
+      }
+			else{
+				console.log('no error');
+			}
+  });
 });
 
 exports.index = function(req, res){
@@ -35,12 +32,18 @@ exports.index = function(req, res){
 
 exports.recentLocation = function(req, res){
 	// Use a timestamp
+	mongo.connect(uri,function(err,db){
+		if(err) {
+			console.log(err);
+			return;
+		}
     db.collection('locations', function(err, collection) {
         collection.find().sort( [['_id', -1]] ).limit(30).toArray(function(err, items) {
             res.send(items);
         });
     });
-};
+	});
+}
 
 exports.addLocation = function(req, res) {
     var location = {};// = req.body;		
@@ -54,24 +57,36 @@ exports.addLocation = function(req, res) {
 			return;
 		}	
     console.log('Adding location: ' + JSON.stringify(location));
-    db.collection('locations', function(err, collection) {
-        collection.insert(location, {safe:true}, function(err, result) {
-            if (err) {
-                res.send({'error':'An error has occurred'});
-            } else {
-                console.log('Success: ' + JSON.stringify(result[0]));
-                res.send(result[0]);
-            }
-        });
-    });
+		mongo.connect(uri,function(err,db){
+			if(err) {
+				console.log(err);
+				return;
+			}		
+			db.collection('locations', function(err, collection) {
+	        collection.insert(location, {safe:true}, function(err, result) {
+	            if (err) {
+	                res.send({'error':'An error has occurred'});
+	            } else {
+	                console.log('Success: ' + JSON.stringify(result[0]));
+	                res.send(result[0]);
+	            }
+	        });
+	    });
+		});
 }
 
 exports.findAllLocations = function(req, res) {
-    db.collection('locations', function(err, collection) {
+	mongo.connect(uri,function(err,db){
+		if(err) {
+			console.log(err);
+			return;
+		}	
+	  db.collection('locations', function(err, collection) {
         collection.find().toArray(function(err, items) {
             res.send(items);
         });
     });
+	});
 };
 
 
